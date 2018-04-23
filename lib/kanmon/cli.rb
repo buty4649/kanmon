85c6aa392b849abcf439ce9f7fae4067e0dcc580
Yuki Koya <ykky@pepabo.com>
2018-04-23 22:31:47 +0900

    Use thor insted of hanami-cli

---
+++ lib/kanmon/cli.rb
@@ -0,0 +1,72 @@
+require "thor"
+
+require "kanmon/myip"
+
+module Kanmon
+  class CLI < Thor
+    class_option :file, aliases: "f", type: :string, default: "kanmon.yml", banner: "FILE", desc: "Load configure from FILE"
+
+    desc "open", "Commands about add rules to SecurityGroup"
+    def open
+      myip = Kanmon::MyIP.get
+      rule = {
+        direction: "ingress",
+        port_range_min: 22,
+        port_range_max: 22,
+        ethertype: "IPv4",
+        protocol: "tcp",
+        remote_ip_prefix: "#{myip}/32",
+        security_group_id: @config["security_group"],
+      }
+
+      Yao::SecurityGroupRule.create(rule)
+      puts "Success!!"
+    end
+
+    desc "close", "Commands about delete rules from SecurityGroup"
+    def close
+      tenant_id = Yao.current_tenant_id
+
+      rule = {
+        direction: "ingress",
+        port_range_min: 22,
+        port_range_max: 22,
+        ethertype: "IPv4",
+        protocol: "tcp",
+        security_group_id: @config["security_group"],
+        tenant_id: tenant_id,
+      }
+
+      result = Yao::SecurityGroupRule.list(rule)
+
+      if result.empty?
+        puts "Not found"
+        exit(1)
+      end
+
+      result.each do |rule|
+        id = rule.id
+        puts "Delete #{id}"
+        Yao::SecurityGroupRule.destroy(id)
+      end
+
+      puts "Success!!"
+    end
+
+    desc "version", "Commands about show version"
+    def version
+      puts Kanmon::VERSION
+    end
+
+    no_commands do
+      def invoke_command(command, *args)
+        unless %w(help version).include?(command.name)
+          Kanmon.init_yao
+          @config = Kanmon.load_config(options[:file])
+        end
+
+        super
+      end
+    end
+  end
+end
