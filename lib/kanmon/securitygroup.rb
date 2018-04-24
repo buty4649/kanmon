fe65d2e1ee1cdc4e36be39d1f407ee45d72a75fe
Yuki Koya <ykky@pepabo.com>
2018-04-24 18:19:08 +0900

    refactor

--- lib/kanmon/securitygroup.rb
+++ lib/kanmon/securitygroup.rb
@@ -7,36 +7,14 @@ module Kanmon
     def initialize(id, ip = nil)
       @id = id
       @ip = ip || Kanmon::MyIP.get
+      @tenant_id = Yao.current_tenant_id
     end
 
     def open
-      rule = {
-        direction: "ingress",
-        port_range_min: 22,
-        port_range_max: 22,
-        ethertype: "IPv4",
-        protocol: "tcp",
-        remote_ip_prefix: "#{@ip}/32",
-        security_group_id: @id,
-      }
-
       Yao::SecurityGroupRule.create(rule)
     end
 
     def close
-      tenant_id = Yao.current_tenant_id
-
-      rule = {
-        direction: "ingress",
-        port_range_min: 22,
-        port_range_max: 22,
-        ethertype: "IPv4",
-        protocol: "tcp",
-        security_group_id: @id,
-        tenant_id: tenant_id,
-        remote_ip_prefix: "#{@ip}/32"
-      }
-
       result = Yao::SecurityGroupRule.list(rule)
       raise "Not found" if result.empty?
 
@@ -45,6 +23,20 @@ module Kanmon
         puts "Delete #{id}"
         Yao::SecurityGroupRule.destroy(id)
       end
+    end
+
+    private
+    def rule
+      {
+        direction: "ingress",
+        port_range_min: 22,
+        port_range_max: 22,
+        ethertype: "IPv4",
+        protocol: "tcp",
+        security_group_id: @id,
+        tenant_id: @tenant_id,
+        remote_ip_prefix: "#{@ip}/32"
+      }
     end
   end
 end
