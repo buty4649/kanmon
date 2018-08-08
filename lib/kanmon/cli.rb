dc7dd2e88b8bbcb5bc43da0d6c40522f41483c34
r_takaishi <ryo.takaishi.0@gmail.com>
2018-08-08 10:30:56 +0900

    support open and close

--- lib/kanmon/cli.rb
+++ lib/kanmon/cli.rb
@@ -3,13 +3,48 @@ require "shellwords"
 
 require "kanmon/securitygroup"
 
+module Yao::Resources
+  class Server < Yao::Resources::Base
+    def self.add_security_group(server_id, security_group_name)
+      action(server_id, {"addSecurityGroup": {"name": security_group_name}})
+    end
+
+    def self.remove_security_group(server_id, security_group_name)
+      action(server_id, {"removeSecurityGroup": {"name": security_group_name}})
+    end
+  end
+end
+
 module Kanmon
   class CLI < Thor
     class_option :kanmon_config, aliases: "f", type: :string, default: "kanmon.yml", banner: "FILE", desc: "Load configure from FILE"
 
     desc "open", "Commands about add rules to SecurityGroup"
     def open
-      @sg.open
+      if @config.key?('security_group')
+        @sg.open
+      end
+
+      if @config.key?('server')
+        server = Yao::Server.get(@config['server'])
+        puts "Create security group allow to access server '#{server.name}'."
+        param = {name: "kanmon-server:#{server.id}-user:#{ENV['OS_USERNAME']}", description: "create by kanmon and #{ENV['OS_USERNAME']}"}
+        sg = Yao::SecurityGroup.create(param)
+        rule = {
+            direction: "ingress",
+            port_range_min: 22,
+            port_range_max: 22,
+            ethertype: "IPv4",
+            protocol: "tcp",
+            security_group_id: sg.id,
+            tenant_id: Yao.current_tenant_id,
+            remote_ip_prefix: "#{Kanmon::MyIP.get}/32"
+        }
+        result = Yao::SecurityGroupRule.create(rule)
+        puts "Create rule to #{sg.name} allow ssh from #{Kanmon::MyIP.get}/32: #{result.id}"
+        p Yao::Server.add_security_group(server.id, sg.name)
+      end
+
       puts "Success!!"
     rescue Yao::Conflict => e
       puts "Is not it already opened?" if e.message.include?("Security group rule already exists.")
@@ -18,7 +53,19 @@ module Kanmon
 
     desc "close", "Commands about delete rules from SecurityGroup"
     def close
-      @sg.close
+      if @config.key?('security_group')
+        @sg.close
+      end
+
+      if @config.key?('server')
+        server = Yao::Server.get(@config['server'])
+        sg_name = "kanmon-server:#{server.id}-user:#{ENV['OS_USERNAME']}"
+        puts "Detach security group #{sg_name} from server '#{server.name}'."
+        Yao::Server.remove_security_group(server.id, sg_name)
+        puts "Delete security group: #{sg_name}"
+        sg = Yao::SecurityGroup.get(sg_name)
+        Yao::SecurityGroup.destroy(sg.id)
+      end
       puts "Success!!"
     end
 
